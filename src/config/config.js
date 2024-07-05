e61678252db6394eaa888100b1af0b3a5688e125
MiguelGL06 <miguel.gl0698@gmail.com>
2024-07-05 00:51:57 -0600

    vercel2 commit2

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
-  dbUser:  process.env.DB_USER,
+/*  dbUser:  process.env.DB_USER,
   dbPassword:  process.env.DB_PASSWORD,
   dbHost:  process.env.DB_HOST,
   dbName:  process.env.DB_NAME,
-  dbPort:  process.env.DB_PORT,
+  dbPort:  process.env.DB_PORT,*/
   apiKey:  process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
   smtpEmail: process.env.SMTP_EMAIL,
