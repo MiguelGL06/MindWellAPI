0a897ccbbef59de62230bb3fbb6d9ac8d41a85e1
MiguelGL06 <miguel.gl0698@gmail.com>
2024-07-06 03:26:10 -0600

    swag commit

--- src/swagger/mainDocs.js
+++ src/swagger/mainDocs.js
@@ -18,6 +18,9 @@ module.exports = {
         },
         {
           "url": "https://mindwellapi.onrender.com"
+        },
+        {
+          "url": "https://mindwellapi-1.onrender.com"
         }
     ],
     "components": {
