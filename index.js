3b38bc1b89e8962f18cf3f3d398588826b6a6795
MiguelGL06 <miguel.gl0698@gmail.com>
2024-07-07 15:06:21 -0600

    cors commit

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-// Importa el módulo express, que es un framework web para Node.js
+// Importa los módulos necesarios
 const express = require('express');
-const mainDocs = require("./src/swagger/mainDocs");
-const swaggerUi = require("swagger-ui-express");
+const mainDocs = require('./src/swagger/mainDocs');
+const swaggerUi = require('swagger-ui-express');
 const sequelize = require('./src/libs/sequelize');
-
 const cors = require('cors');
-// Importa el módulo routerAPI desde el archivo routes.js ubicado en la misma carpeta
 const routerAPI = require('./src/routes/index');
-// Importa los manejadores de errores desde el archivo error.handler.js ubicado en la carpeta middlewares
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./src/middlewares/error.handler');
-// Importa la función checkApiKey desde el archivo auth.handler.js ubicado en la carpeta middlewares
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+  ormErrorHandler,
+} = require('./src/middlewares/error.handler');
 const { checkApiKey } = require('./src/middlewares/auth.handler');
+
 // Crea una instancia de la aplicación express
 const app = express();
 
+// Configura CORS
 const corsOptions = {
   origin: 'http://localhost:3000', // Cambia esto a la URL de tu front-end
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
 };
-
 app.use(cors(corsOptions));
+
 // Define el número de puerto en el que se ejecutará el servidor
 const port = 3000;
+
 // Agrega un middleware para analizar el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
 
+// Configura Swagger UI para la documentación de la API
 app.use(
-  "/api-docs",
+  '/api-docs',
   swaggerUi.serve,
   swaggerUi.setup(mainDocs, {
-          customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
-          customCss: ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }"
-      }
-  )
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css',
+    customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  })
 );
 
 // Importa y ejecuta el archivo auth.js ubicado en la carpeta utils
 require('./src/utils/auth/core');
 
-// Maneja las solicitudes GET en la ruta raíz ("/") con el middleware checkApiKey
-app.get('/',(req, res) => {
+// Define la ruta raíz
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Llama al enrutador definido en routes.js y pasa la instancia de la aplicación express como argumento
+// Configura las rutas de la API
 routerAPI(app);
 
 // Agrega los middlewares de manejo de errores en el orden especificado
